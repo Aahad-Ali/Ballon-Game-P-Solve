994622fc524ab8443d5915a7d529cc18642aabed
Aahad Ali <104500845+Aahad-Ali@users.noreply.github.com>
2022-08-11 16:14:57 +0500

    Add files via upload

---
+++ ballon game final/app.js
@@ -0,0 +1,593 @@
+var colours = ["red", "blue", "green", "orange", "yellow", "brown", "indigo"];
+console.log("hello");
+for (var i = 1; i < 31; i++) {
+    var randomNumber = Math.round(Math.random() * 6);
+    var randomColour = colours[randomNumber];
+    document.getElementById(`${i}`).style.backgroundColor = randomColour;
+}
+function randomColourGenerating() {
+    var randomColourPicker = Math.round(Math.random() * 6);
+    window.randomColourPicker2 = colours[randomColourPicker];
+    document.getElementById("color").innerText = window.randomColourPicker2;
+}
+randomColourGenerating();
+
+var userScore = 0;
+document.getElementById("score").innerText = userScore;
+
+function show(args) {
+
+    if (args === "1") {
+
+        if (document.getElementById("1").style.backgroundColor === randomColourPicker2) {
+            document.getElementById("1").style.visibility = "hidden";
+            userScore = userScore + 1;
+            document.getElementById("score").innerText = userScore;
+            randomColourGenerating();
+
+            if (userScore === 25) {
+                document.getElementById("main").innerText = "You 'WIN' the Game  PLZ restart the Game";
+            }
+        }
+
+        else {
+            document.getElementById("1").style.visibility = "visible";
+        }
+
+    }
+
+    if (args === "2") {
+
+        if (document.getElementById("2").style.backgroundColor === randomColourPicker2) {
+            document.getElementById("2").style.visibility = "hidden";
+            userScore = userScore + 1;
+            document.getElementById("score").innerText = userScore;
+            randomColourGenerating();
+
+            if (userScore === 25) {
+                document.getElementById("main").innerText = "You 'WIN' the Game  PLZ restart the Game";
+            }
+        }
+
+        else {
+            document.getElementById("2").style.visibility = "visible";
+        }
+
+    }
+
+    if (args === "3") {
+
+        if (document.getElementById("3").style.backgroundColor === randomColourPicker2) {
+            document.getElementById("3").style.visibility = "hidden";
+            userScore = userScore + 1;
+            document.getElementById("score").innerText = userScore;
+            randomColourGenerating();
+
+            if (userScore === 25) {
+                document.getElementById("main").innerText = "You 'WIN' the Game  PLZ restart the Game";
+            }
+        }
+
+        else {
+            document.getElementById("3").style.visibility = "visible";
+        }
+
+    }
+
+    if (args === "4") {
+
+        if (document.getElementById("4").style.backgroundColor === randomColourPicker2) {
+            document.getElementById("4").style.visibility = "hidden";
+            userScore = userScore + 1;
+            document.getElementById("score").innerText = userScore;
+            randomColourGenerating();
+
+            if (userScore === 25) {
+                document.getElementById("main").innerText = "You 'WIN' the Game  PLZ restart the Game";
+            }
+        }
+
+        else {
+            document.getElementById("4").style.visibility = "visible";
+        }
+
+    }
+
+    if (args === "5") {
+
+        if (document.getElementById("5").style.backgroundColor === randomColourPicker2) {
+            document.getElementById("5").style.visibility = "hidden";
+            userScore = userScore + 1;
+            document.getElementById("score").innerText = userScore;
+            randomColourGenerating();
+
+            if (userScore === 25) {
+                document.getElementById("main").innerText = "You 'WIN' the Game  PLZ restart the Game";
+            }
+        }
+
+        else {
+            document.getElementById("5").style.visibility = "visible";
+        }
+
+    }
+
+    if (args === "6") {
+
+        if (document.getElementById("6").style.backgroundColor === randomColourPicker2) {
+            document.getElementById("6").style.visibility = "hidden";
+            userScore = userScore + 1;
+            document.getElementById("score").innerText = userScore;
+            randomColourGenerating();
+
+            if (userScore === 25) {
+                document.getElementById("main").innerText = "You 'WIN' the Game  PLZ restart the Game";
+            }
+        }
+
+        else {
+            document.getElementById("6").style.visibility = "visible";
+        }
+
+    }
+
+    if (args === "7") {
+
+        if (document.getElementById("7").style.backgroundColor === randomColourPicker2) {
+            document.getElementById("7").style.visibility = "hidden";
+            userScore = userScore + 1;
+            document.getElementById("score").innerText = userScore;
+            randomColourGenerating();
+
+            if (userScore === 25) {
+                document.getElementById("main").innerText = "You 'WIN' the Game  PLZ restart the Game";
+            }
+        }
+
+        else {
+            document.getElementById("7").style.visibility = "visible";
+        }
+
+    }
+
+    if (args === "8") {
+
+        if (document.getElementById("8").style.backgroundColor === randomColourPicker2) {
+            document.getElementById("8").style.visibility = "hidden";
+            userScore = userScore + 1;
+            document.getElementById("score").innerText = userScore;
+            randomColourGenerating();
+
+            if (userScore === 25) {
+                document.getElementById("main").innerText = "You 'WIN' the Game  PLZ restart the Game";
+            }
+        }
+
+        else {
+            document.getElementById("8").style.visibility = "visible";
+        }
+
+    }
+
+    if (args === "9") {
+
+        if (document.getElementById("9").style.backgroundColor === randomColourPicker2) {
+            document.getElementById("9").style.visibility = "hidden";
+            userScore = userScore + 1;
+            document.getElementById("score").innerText = userScore;
+            randomColourGenerating();
+
+            if (userScore === 25) {
+                document.getElementById("main").innerText = "You 'WIN' the Game  PLZ restart the Game";
+            }
+        }
+
+        else {
+            document.getElementById("9").style.visibility = "visible";
+        }
+
+    }
+
+    if (args === "10") {
+
+        if (document.getElementById("10").style.backgroundColor === randomColourPicker2) {
+            document.getElementById("10").style.visibility = "hidden";
+            userScore = userScore + 1;
+            document.getElementById("score").innerText = userScore;
+            randomColourGenerating();
+
+            if (userScore === 25) {
+                document.getElementById("main").innerText = "You 'WIN' the Game  PLZ restart the Game";
+            }
+        }
+
+        else {
+            document.getElementById("10").style.visibility = "visible";
+        }
+
+    }
+
+    if (args === "11") {
+
+        if (document.getElementById("11").style.backgroundColor === randomColourPicker2) {
+            document.getElementById("11").style.visibility = "hidden";
+            userScore = userScore + 1;
+            document.getElementById("score").innerText = userScore;
+            randomColourGenerating();
+
+            if (userScore === 25) {
+                document.getElementById("main").innerText = "You 'WIN' the Game  PLZ restart the Game";
+            }
+        }
+
+        else {
+            document.getElementById("11").style.visibility = "visible";
+        }
+
+    }
+
+    if (args === "12") {
+
+        if (document.getElementById("12").style.backgroundColor === randomColourPicker2) {
+            document.getElementById("12").style.visibility = "hidden";
+            userScore = userScore + 1;
+            document.getElementById("score").innerText = userScore;
+            randomColourGenerating();
+
+            if (userScore === 25) {
+                document.getElementById("main").innerText = "You 'WIN' the Game  PLZ restart the Game";
+            }
+        }
+
+        else {
+            document.getElementById("12").style.visibility = "visible";
+        }
+
+    }
+
+    if (args === "13") {
+
+        if (document.getElementById("13").style.backgroundColor === randomColourPicker2) {
+            document.getElementById("13").style.visibility = "hidden";
+            userScore = userScore + 1;
+            document.getElementById("score").innerText = userScore;
+            randomColourGenerating();
+
+            if (userScore === 25) {
+                document.getElementById("main").innerText = "You 'WIN' the Game  PLZ restart the Game";
+            }
+        }
+
+        else {
+            document.getElementById("13").style.visibility = "visible";
+        }
+
+    }
+
+    if (args === "14") {
+
+        if (document.getElementById("14").style.backgroundColor === randomColourPicker2) {
+            document.getElementById("14").style.visibility = "hidden";
+            userScore = userScore + 1;
+            document.getElementById("score").innerText = userScore;
+            randomColourGenerating();
+
+            if (userScore === 25) {
+                document.getElementById("main").innerText = "You 'WIN' the Game  PLZ restart the Game";
+            }
+        }
+
+        else {
+            document.getElementById("14").style.visibility = "visible";
+        }
+
+    }
+
+    if (args === "15") {
+
+        if (document.getElementById("15").style.backgroundColor === randomColourPicker2) {
+            document.getElementById("15").style.visibility = "hidden";
+            userScore = userScore + 1;
+            document.getElementById("score").innerText = userScore;
+            randomColourGenerating();
+
+            if (userScore === 25) {
+                document.getElementById("main").innerText = "You 'WIN' the Game  PLZ restart the Game";
+            }
+        }
+
+        else {
+            document.getElementById("15").style.visibility = "visible";
+        }
+
+    }
+
+    if (args === "16") {
+
+        if (document.getElementById("16").style.backgroundColor === randomColourPicker2) {
+            document.getElementById("16").style.visibility = "hidden";
+            userScore = userScore + 1;
+            document.getElementById("score").innerText = userScore;
+            randomColourGenerating();
+
+            if (userScore === 25) {
+                document.getElementById("main").innerText = "You 'WIN' the Game  PLZ restart the Game";
+            }
+        }
+
+        else {
+            document.getElementById("16").style.visibility = "visible";
+        }
+
+    }
+
+    if (args === "17") {
+
+        if (document.getElementById("17").style.backgroundColor === randomColourPicker2) {
+            document.getElementById("17").style.visibility = "hidden";
+            userScore = userScore + 1;
+            document.getElementById("score").innerText = userScore;
+            randomColourGenerating();
+
+            if (userScore === 25) {
+                document.getElementById("main").innerText = "You 'WIN' the Game  PLZ restart the Game";
+            }
+        }
+
+        else {
+            document.getElementById("17").style.visibility = "visible";
+        }
+
+    }
+
+    if (args === "18") {
+
+        if (document.getElementById("18").style.backgroundColor === randomColourPicker2) {
+            document.getElementById("18").style.visibility = "hidden";
+            userScore = userScore + 1;
+            document.getElementById("score").innerText = userScore;
+            randomColourGenerating();
+
+            if (userScore === 25) {
+                document.getElementById("main").innerText = "You 'WIN' the Game  PLZ restart the Game";
+            }
+        }
+
+        else {
+            document.getElementById("18").style.visibility = "visible";
+        }
+
+    }
+
+    if (args === "19") {
+
+        if (document.getElementById("19").style.backgroundColor === randomColourPicker2) {
+            document.getElementById("19").style.visibility = "hidden";
+            userScore = userScore + 1;
+            document.getElementById("score").innerText = userScore;
+            randomColourGenerating();
+
+            if (userScore === 25) {
+                document.getElementById("main").innerText = "You 'WIN' the Game  PLZ restart the Game";
+            }
+        }
+
+        else {
+            document.getElementById("19").style.visibility = "visible";
+        }
+
+    }
+
+    if (args === "20") {
+
+        if (document.getElementById("20").style.backgroundColor === randomColourPicker2) {
+            document.getElementById("20").style.visibility = "hidden";
+            userScore = userScore + 1;
+            document.getElementById("score").innerText = userScore;
+            randomColourGenerating();
+
+            if (userScore === 25) {
+                document.getElementById("main").innerText = "You 'WIN' the Game  PLZ restart the Game";
+            }
+        }
+
+        else {
+            document.getElementById("20").style.visibility = "visible";
+        }
+
+    }
+
+    if (args === "21") {
+
+        if (document.getElementById("21").style.backgroundColor === randomColourPicker2) {
+            document.getElementById("21").style.visibility = "hidden";
+            userScore = userScore + 1;
+            document.getElementById("score").innerText = userScore;
+            randomColourGenerating();
+
+            if (userScore === 25) {
+                document.getElementById("main").innerText = "You 'WIN' the Game  PLZ restart the Game";
+            }
+        }
+
+        else {
+            document.getElementById("21").style.visibility = "visible";
+        }
+
+    }
+
+    if (args === "22") {
+
+        if (document.getElementById("22").style.backgroundColor === randomColourPicker2) {
+            document.getElementById("22").style.visibility = "hidden";
+            userScore = userScore + 1;
+            document.getElementById("score").innerText = userScore;
+            randomColourGenerating();
+
+            if (userScore === 25) {
+                document.getElementById("main").innerText = "You 'WIN' the Game  PLZ restart the Game";
+            }
+        }
+
+        else {
+            document.getElementById("22").style.visibility = "visible";
+        }
+
+    }
+
+    if (args === "23") {
+
+        if (document.getElementById("23").style.backgroundColor === randomColourPicker2) {
+            document.getElementById("23").style.visibility = "hidden";
+            userScore = userScore + 1;
+            document.getElementById("score").innerText = userScore;
+            randomColourGenerating();
+
+            if (userScore === 25) {
+                document.getElementById("main").innerText = "You 'WIN' the Game  PLZ restart the Game";
+            }
+        }
+
+        else {
+            document.getElementById("23").style.visibility = "visible";
+        }
+
+    }
+
+    if (args === "24") {
+
+        if (document.getElementById("24").style.backgroundColor === randomColourPicker2) {
+            document.getElementById("24").style.visibility = "hidden";
+            userScore = userScore + 1;
+            document.getElementById("score").innerText = userScore;
+            randomColourGenerating();
+
+            if (userScore === 25) {
+                document.getElementById("main").innerText = "You 'WIN' the Game  PLZ restart the Game";
+            }
+        }
+
+        else {
+            document.getElementById("24").style.visibility = "visible";
+        }
+
+    }
+
+    if (args === "25") {
+
+        if (document.getElementById("25").style.backgroundColor === randomColourPicker2) {
+            document.getElementById("25").style.visibility = "hidden";
+            userScore = userScore + 1;
+            document.getElementById("score").innerText = userScore;
+            randomColourGenerating();
+
+            if (userScore === 25) {
+                document.getElementById("main").innerText = "You 'WIN' the Game  PLZ restart the Game";
+            }
+        }
+
+        else {
+            document.getElementById("25").style.visibility = "visible";
+        }
+
+    }
+    if (args === "26") {
+
+        if (document.getElementById("26").style.backgroundColor === randomColourPicker2) {
+            document.getElementById("26").style.visibility = "hidden";
+            userScore = userScore + 1;
+            document.getElementById("score").innerText = userScore;
+            randomColourGenerating();
+
+            if (userScore === 25) {
+                document.getElementById("main").innerText = "You 'WIN' the Game  PLZ restart the Game";
+            }
+        }
+
+        else {
+            document.getElementById("26").style.visibility = "visible";
+        }
+
+    }
+
+    if (args === "27") {
+
+        if (document.getElementById("27").style.backgroundColor === randomColourPicker2) {
+            document.getElementById("27").style.visibility = "hidden";
+            userScore = userScore + 1;
+            document.getElementById("score").innerText = userScore;
+            randomColourGenerating();
+
+            if (userScore === 25) {
+                document.getElementById("main").innerText = "You 'WIN' the Game  PLZ restart the Game";
+            }
+        }
+
+        else {
+            document.getElementById("27").style.visibility = "visible";
+        }
+
+    }
+
+    if (args === "28") {
+
+        if (document.getElementById("28").style.backgroundColor === randomColourPicker2) {
+            document.getElementById("28").style.visibility = "hidden";
+            userScore = userScore + 1;
+            document.getElementById("score").innerText = userScore;
+            randomColourGenerating();
+
+            if (userScore === 25) {
+                document.getElementById("main").innerText = "You 'WIN' the Game  PLZ restart the Game";
+            }
+        }
+
+        else {
+            document.getElementById("28").style.visibility = "visible";
+        }
+
+    }
+
+    if (args === "29") {
+
+        if (document.getElementById("29").style.backgroundColor === randomColourPicker2) {
+            document.getElementById("29").style.visibility = "hidden";
+            userScore = userScore + 1;
+            document.getElementById("score").innerText = userScore;
+            randomColourGenerating();
+
+            if (userScore === 25) {
+                document.getElementById("main").innerText = "You 'WIN' the Game  PLZ restart the Game";
+            }
+        }
+
+        else {
+            document.getElementById("29").style.visibility = "visible";
+        }
+
+    }
+
+    if (args === "30") {
+
+        if (document.getElementById("30").style.backgroundColor === randomColourPicker2) {
+            document.getElementById("30").style.visibility = "hidden";
+            userScore = userScore + 1;
+            document.getElementById("score").innerText = userScore;
+            randomColourGenerating();
+
+            if (userScore === 25) {
+                document.getElementById("main").innerText = "You 'WIN' the Game  PLZ restart the Game";
+            }
+        }
+
+        else {
+            document.getElementById("30").style.visibility = "visible";
+        }
+
+    }
+
+
+}
+function change() {
+    randomColourGenerating();
+}
